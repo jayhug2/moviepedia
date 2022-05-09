5ca822fe7cb28af8980a40c608ac8d5405b32dbc
jayhug2 <jayhug2@daum.net>
2022-05-10 04:31:09 +0900

    api update

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,15 +1,20 @@
 import { useState } from 'react';
+import { createReview } from '../api';
 import FileInput from './FileInput';
+import RatingInput from './RatingInput';
 import './ReviewForm.css';
 
+const INITIAL_VALUES = {
+  title: '',
+  rating: 0,
+  content: '',
+  imgFile: null,
+}
 
 function ReviewForm() {
-    const [values, setValues] = useState({
-      title: '',
-      rating: 0,
-      content: '',
-      imgFile: null,
-    })
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [submittingError, setSubmittingError ] = useState(null);
+    const [values, setValues] = useState(INITIAL_VALUES)
 
 
     const handleChange = (name, value) => {
@@ -24,11 +29,24 @@ function ReviewForm() {
       handleChange(name, value);
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
       e.preventDefault();
-      console.log({
-        values
-      })
+      const formData = new FormData();
+      formData.append('title', values.title);
+      formData.append('rating', values.rating);
+      formData.append('content', values.content);
+      formData.append('imgFile', values.imgFile);
+      try {
+        setSubmittingError(null);
+        setIsSubmitting(true);
+        await createReview(formData);
+      } catch (error) {
+        setSubmittingError(error);
+        return;
+      } finally {
+        setIsSubmitting(false);
+      }
+      setValues(INITIAL_VALUES);
     }
 
     return (
@@ -39,10 +57,9 @@ function ReviewForm() {
             onChange={handleInputChange}
             name="title"
           />
-          <input 
-            type="number"
+          <RatingInput
             value={values.rating}
-            onChange={handleInputChange}
+            onChange={handleChange}
             name="rating"
           />
           <textarea 
@@ -50,7 +67,8 @@ function ReviewForm() {
             onChange={handleInputChange}
             name="content"
           />
-          <button type="submit">확인</button>
+          <button type="submit" disabled={isSubmitting}>확인</button>
+          {submittingError?.message && <div>{submittingError.message}</div>}
         </form>
     );
 }
